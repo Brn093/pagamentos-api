6601c2c65f9be0a8c19926a087bf972d277c1b18
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-12-01 15:00:58 -0300

    Criando o projeto da API

---
+++ src/test/java/com/example/pagamentos/api/PagamentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pagamentos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
